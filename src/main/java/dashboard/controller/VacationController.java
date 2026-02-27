09a3baf2a31a7500cab48ec1f9ef5323663056f8
Min0522-sku <wkdalstj0522@gmail.com>
2026-02-27 18:19:01 +0900

    feat: vacation 백엔드 완성

--- src/main/java/dashboard/controller/VacationController.java
+++ src/main/java/dashboard/controller/VacationController.java
@@ -1,4 +1,34 @@
 package dashboard.controller;
 
+import dashboard.model.dao.VacationDao;
+import dashboard.model.dto.VacationDto;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/dashboard/vacation")
 public class VacationController {
+    @Autowired private VacationDao vacationDao;
+
+    @PostMapping
+    public boolean vPost(@RequestBody VacationDto vacationDto){
+        boolean result = vacationDao.vPost(vacationDto);
+        return result;
+    }
+
+    @GetMapping
+    public List<VacationDto> vFindAll(){
+        List<VacationDto> result = vacationDao.vFindAll();
+        return result;
+    }
+
+    @DeleteMapping
+    public boolean dDelete(@RequestParam int vcode){
+        boolean result = vacationDao.vDelete(vcode);
+        return result;
+    }
+
+
 }
