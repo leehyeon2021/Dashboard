204d209973aab075cf56f95272aeed481e7055c5
Min0522-sku <wkdalstj0522@gmail.com>
2026-02-27 15:52:42 +0900

    feat: 롬복 설정

--- src/main/java/dashboard/model/dto/departmentDto.java
+++ src/main/java/dashboard/model/dto/departmentDto.java
@@ -1,4 +1,14 @@
 package dashboard.model.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
 public class departmentDto {
+    private Integer dcode;
+    private String dname;
 }
