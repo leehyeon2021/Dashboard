44f3ebbe77c1270c659405c0ecc65f15d714c4ec
Min0522-sku <wkdalstj0522@gmail.com>
2026-03-03 16:11:03 +0900

    feat: 전체휴가목록 출력 기능 완성

--- src/main/resources/static/vacation.js
+++ src/main/resources/static/vacation.js
@@ -1,24 +1,20 @@
 console.log("vacation.js exe")
 
 const vFindAll = async () => {
-    const tbody = document.querySelector("#right_bottom_inner");
-    let html = "";
-    const response = await axios.get("/dashboard/vacation");
+    try{
+        const response = await axios.get("/dashboard/vacation");
+        const data = response.data;
 
-    const data = response.data;
-    for (let i = 0; i <= data.length-1; i++){
-        const vacation = data[i];
-        let staffName = ""; // 사원명을 가져오기위한 변수
-        for(let j = 0; j < staff.length; j++){ // 사원 배열을 순회
-            if(vacation.scode == staff[j].scode){ // 일치하는 사원코드을 찾으면
-                staffName = staff[j].staffName; //사원명 저장
-                break;
-            }
-        }
-        html += `
+        const tbody = document.querySelector("#right_bottom_inner");
+        let html = "";
+
+        for (let i = 0; i < data.length; i++) {
+            const vacation = data[i];
+            html += `
                 <div class="box">
                     <div class="line1">
-                        <div>${staffName}</div><button class="vacationDel" onclick="vDelete(${vacation.vcode})">신청취소</button>
+                        <div>${vacation.sname}</div>
+                        <button class="vacationDel" onclick="vDelete(${vacation.vcode})">신청취소</button>
                     </div>
                     <div class="line2">
                         ${vacation.vstart}~${vacation.vend}
@@ -28,9 +24,11 @@ const vFindAll = async () => {
                     </div>
                 </div>
         `;
-
+        }
+        tbody.innerHTML = html;
+    }catch (e) {
+        console.error(e);
     }
-    tbody.innerHTML = html;
 }
 vFindAll();
 
@@ -59,12 +57,12 @@ const vPost = async () =>{
     }
 }
 
-const vDelete = async ( dcode ) =>{
+const vDelete = async ( vcode ) =>{
 
-    const response = await axios.delete(`/dashboard/department?dcode=${dcode}`);
+    const response = await axios.delete(`/dashboard/department?vcode=${vcode}`);
     const data = response.data;
     if(data == true){
         alert("삭제성공")
-        dFindAll();
+        vFindAll();
     }else{alert("삭제실패")}
 }
